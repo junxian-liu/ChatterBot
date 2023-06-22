4c5ee36d9817dddde28250aff1eeacae1968db42
Junxian Liu <liujunxian134@gmail.com>
2023-06-21 23:41:21 -0700

    Initial

---
+++ bot.py
@@ -0,0 +1,47 @@
+# bot.py
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from cleaner import clean_corpus
+
+CHAT = "chat.txt"
+
+chatbot = ChatBot(
+    "Junxian's Bot",
+    logic_adapters=[
+        {
+            "import_path": "chatterbot.logic.BestMatch"
+        },
+        {
+            "import_path": "chatterbot.logic.MathematicalEvaluation"
+        },
+        {
+            "import_path": "stock_adapter.MyLogicAdapter"
+        }
+    ]
+)
+
+#TRAINING
+trainer = ListTrainer(chatbot)
+#Personal training
+# cleaned_corpus = clean_corpus(CHAT)
+# trainer.train(cleaned_corpus)
+
+trainer.train([
+    "Hi",
+    "Welcome, friend 🤗",
+])
+trainer.train([
+    "How are you today?",
+    "I am doing good, what about you?",
+])
+
+#CHAT
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"Junxian's Bot: {chatbot.get_response(query)}")
---
+++ stock_adapter.py
@@ -0,0 +1,70 @@
+#Generate responses regarding stock
+from chatterbot.logic import LogicAdapter
+from polygon import RESTClient
+
+class MyLogicAdapter(LogicAdapter):
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+
+    def can_process(self, statement):
+        s = 'stock'
+        d = '$'
+        if statement.text.startswith(d): 
+            return True
+        else:
+            return False
+
+    def process(self, input_statement, additional_response_selection_parameters):
+        import random
+        import json
+        from typing import cast
+        from urllib3 import HTTPResponse
+
+        # Randomly select a confidence between 0 and 1
+        confidence = random.uniform(0, 1)
+
+        # Make call to Polygon API
+        API_KEY = 'Ao9fHerRTi5M9GteFUozhRdOasDXUXfN'
+        client = RESTClient(API_KEY)
+        aggs = cast(
+            HTTPResponse, 
+            client.get_aggs(
+                'APPL', 
+                'day', 
+                '2022-05-20', 
+                '2022-11-11', 
+                raw = True
+            )
+        )
+
+        data = json.loads(aggs.data)
+
+        # Getting closing prices of stock
+        for item in data:
+            if item == 'results':
+                rawData = data[item]
+
+        closingPrice = []
+        for bar in rawData:
+            for category in bar:
+                if category == 'c':
+                    closingPrice.append(bar[category])
+
+        lengthOfClosingPrice = len(closingPrice)
+        sumOfClosingPrice = 0
+        max = 0
+        min = 999999
+        for i in range(lengthOfClosingPrice):
+            sumOfClosingPrice += closingPrice[i]
+            if(closingPrice[i] > max):
+                max = closingPrice[i]
+            if(closingPrice[i] < min):
+                min = closingPrice[i]
+        
+        mean = sumOfClosingPrice / lengthOfClosingPrice
+        # For this example, we will just return the input as output
+        input_statement = "For the ticker $APPL, the mean price was {mean}, the max price was {max}, and the min price was {min}"
+        selected_statement = input_statement.format(mean = mean, max = max, min = min)
+        selected_statement.confidence = confidence
+
+        return selected_statement
